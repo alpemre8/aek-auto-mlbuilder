5ec7bf525036a01d851c4517d17e6133ac9db973
Alp Emre Karaahmet <alpemrekaraahmet@gmail.com>
2025-09-08 15:59:02 +0300

    update readme and usage

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aek-auto-mlbuilder",        # PyPI dağıtım adı
-    version="0.1.0",
+    version="0.1.1",
     description="Automatic ML model builder in Python",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -12,5 +12,9 @@ setup(
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.8",
-    install_requires=[],  # bağımlılıkları buraya eklersin
+    install_requires=[
+        "scikit-learn>=1.0",
+        "numpy>=1.23",
+        "pandas>=1.5"
+    ],  # bağımlılıkları buraya eklersin
 )
