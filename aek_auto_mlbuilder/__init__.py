7e84d2f8359f60ce908c046cdb303a9d9c76ee12
Alp Emre Karaahmet <alpemrekaraahmet@gmail.com>
2025-09-09 16:24:50 +0300

    knn added

--- aek_auto_mlbuilder/__init__.py
+++ aek_auto_mlbuilder/__init__.py
@@ -3,3 +3,4 @@ from .utils import split_data
 from .linear_regression import LinearRegressor
 from .logistic_regression import LogisticClassifier
 from .decision_tree import DecisionTreeModel
+from. knn import KNNModel
