e917daf2d735132becee9ad9f7a6d8cd948192a2
Alp Emre Karaahmet <alpemrekaraahmet@gmail.com>
2025-09-18 16:11:59 +0300

    nusvc bug was fixed

--- aek_auto_mlbuilder/nusvc.py
+++ aek_auto_mlbuilder/nusvc.py
@@ -10,9 +10,8 @@ class NuSVCModel(BaseModel):
         super().__init__()
         self.param_grid = param_grid or {
             "nu": [0.25, 0.5, 0.75],
-            "kernel": ["linear", "poly", "rbf", "sigmoid"],
-            "C": [0.1, 1, 10]
-        }
+            "kernel": ["linear", "poly", "rbf", "sigmoid"]
+            }
     
     def train(self, X, y):
         best_score = -float("inf")
@@ -20,17 +19,15 @@ class NuSVCModel(BaseModel):
 
         for nu in self.param_grid["nu"]:
             for kernel in self.param_grid["kernel"]:
-                for C in self.param_grid["C"]:
-                    model = NuSVC(
-                        nu=nu,
-                        kernel=kernel,
-                        C=C
+                model = NuSVC(
+                    nu=nu,
+                    kernel=kernel
                     )
-                    model.fit(X, y)
-                    score = model.score(X, y)
-                    if score > best_score:
-                        best_score = score
-                        best_model = model
+                model.fit(X, y)
+                score = model.score(X, y)
+                if score > best_score:
+                    best_score = score
+                    best_model = model
         
         self.best_model = best_model
         self.best_score = best_score
